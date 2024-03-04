5d6c8a8edca4d84277c19b629b7148a646a9e1c2
Ruliane <ruliane@ruliane.net>
2024-03-04 16:28:49 +0100

    Add two new transforms : MultiValueAnd and MultiValueOr

---
+++ fim.mare/Model/Transforms/Transform.MultiValueAnd.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace FIM.MARE
+{
+    public class MultiValueAnd : Transform
+    {
+        public override object Convert(object value)
+        {
+            if (value == null) return value;
+
+            List<object> values = FromValueCollection(value);
+            List<object> returnValues = new List<object>();
+
+            foreach (object val in values)
+            {
+                if (!bool.Parse(val as string))
+                {
+                    returnValues.Add(false);
+                    return returnValues;
+                }
+            }
+            returnValues.Add(true);
+            return returnValues;
+        }
+    }
+}
---
+++ fim.mare/Model/Transforms/Transform.MultiValueOr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace FIM.MARE
+{
+    public class MultiValueOr : Transform
+    {
+        public override object Convert(object value)
+        {
+            if (value == null) return value;
+
+            List<object> values = FromValueCollection(value);
+            List<object> returnValues = new List<object>();
+
+            foreach (object val in values)
+            {
+                if (bool.Parse(val as string))
+                {
+                    returnValues.Add(true);
+                    return returnValues;
+                }
+            }
+            returnValues.Add(false);
+            return returnValues;
+        }
+    }
+}
